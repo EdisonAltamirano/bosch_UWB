8a2e43f3c14083ebdd3df189536071a75f09d044
Rakshit Sushanta (CR/RPS1-NA) <rau1pal@bosch.com>
2026-05-04 14:37:41 -0700

    Reset command and system state messages added

--- uwb_sw/Core/Inc/udp_server.h
+++ uwb_sw/Core/Inc/udp_server.h
@@ -17,6 +17,8 @@
 #define CMD_CIR_REPORT				0x02
 #define CMD_UDP_ETH_ACK				0x03
 #define CMD_ERROR_REPORT			0x04
+#define CMD_SYSTEM_COMMAND			0x05
+#define CMD_SYSTEM_STATE			0x06
 
 /* Error ids for udp_err_report_cmd_t */
 #define ERR_INIT_UWB_SUBSYSTEM_FAILED			0x03
@@ -26,6 +28,13 @@
 #define ERR_UWB_RADAR_SESSION_STOP_FAILED		0x0D
 #define ERR_UWB_RADAR_SESSION_DEINIT_FAILED		0x0E
 #define ERR_UNKNOWN_USER_CMD_RECEIVED			0x0F
+#define ERR_UNKNOWN_SYS_CMD_RECEIVED			0x10
+
+/* System state commands */
+#define CMD_SYSTEM_RESET						0x01
+#define CMD_SYSTEM_STATE_OK						0x00
+#define CMD_SYSTEM_STATE_NOK					0x01
+#define CMD_SYSTEM_STATE_RADAR_SESSION_DONE		0x02
 
 void nucleo_udp_init(void);
 uint8_t nucleo_udp_send(u16_t port, u8_t cmd_id, const u8_t *payload, u16_t payload_length);
